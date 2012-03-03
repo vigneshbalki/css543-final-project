c98171fc5f1c0e34f9c4a1b0006f298d3e340f46
Dave <david.c.hunn@gmail.com>
2012-03-03 13:07:55 -0800

    Added some rmi stuff.

---
+++ Homework_4/src/java/MobileTracker/RouteServerInterface.java
@@ -0,0 +1,14 @@
+package MobileTracker;
+
+import java.rmi.*;
+import java.util.Set;
+
+public interface RouteServerInterface extends Remote {
+
+  public Set<String> getRouteNameSet(/*String user_name*/) 
+          throws RemoteException;
+  
+  public String getRoute(int routeID) throws RemoteException;
+  
+  public void addRoute(int routeID, Route r) throws RemoteException;
+}
