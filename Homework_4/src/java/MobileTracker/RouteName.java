440d04dd645d4647d72536e9bc4b8ecb95351e19
Dave <david.c.hunn@gmail.com>
2012-03-03 19:56:35 -0800

    added RouteName class

---
+++ Homework_4/src/java/MobileTracker/RouteName.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MobileTracker;
+
+/**
+ *
+ * @author Dave
+ */
+public class RouteName {
+  String name;
+  Integer id;
+}
